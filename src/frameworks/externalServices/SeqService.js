0e40202fc78b0fdca4ff67e0879515f5c0d72fdd
andreschica <oachicav@gmail.com>
2022-05-20 20:17:21 -0500

    seq service

--- src/frameworks/externalServices/SeqService.js
+++ src/frameworks/externalServices/SeqService.js
@@ -2,7 +2,7 @@
 const { SeqTransport } = require('@datalust/winston-seq');
 
 module.exports  = SeqService = new SeqTransport({
-    serverUrl: "http://10.43.102.56:5341",
+    serverUrl: "http://34.132.151.243:80",
     apiKey: "A0O03qZS7UezNmGXEze1",
     onError: (e => { console.error(e) }),
   })
